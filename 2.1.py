a5c24a67f51fea2d777ffa7663d53f8f8df74d74
AshutoshTiwari-187 <89823219+AshutoshTiwari-187@users.noreply.github.com>
2023-02-04 23:40:10 +0530

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the marks:"))
+if  a>90:
+    print("Grade -> A")
+elif a<=90 and a>80:
+    print("Grade -> B")
+elif a<=80 and a>=60:
+    print("Grade -> c")
+elif a<60:
+    print("Grade -> D")
